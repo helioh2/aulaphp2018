bc575992d3c2e01cc67f772a6858d2bbb7ee058c
Helio <heliohenrique3@gmail.com>
2018-06-13 18:52:37 -0300

    CRUD server working. Augmenting and refactoring of UI

---
+++ config/model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+mongoose.connect('mongodb://localhost:27017/clientes')
+
+const itemSchema = new Schema({
+    nome: {type: String, required: true},
+    preco: {type: Number, required: true}
+})
+
+const pedidoSchema = new Schema({
+    quantidade: {type: Number, required: true},
+    frete: Number,
+    item: {type: Schema.ObjectId, ref: 'Item', required: true}
+})
+
+const clienteSchema = new Schema({
+    nome: {type: String, required: true}, 
+    idade: Number, 
+    sexo: {type: String, enum: ['M', 'F']}, 
+    // dataNascimento: Date, 
+    email: String, 
+    endereco: String,
+    pedidos: [pedidoSchema]
+    // pedidos: [{type: Schema.ObjectId, ref: 'Pedido'}]
+});
+
+const Item = mongoose.model('Item', itemSchema)
+// const Pedido = mongoose.model('Pedido', clienteSchema)
+const Cliente = mongoose.model('Cliente', clienteSchema)
+
+module.exports = {Item, Cliente};
--- server.js
+++ server.js
@@ -3,26 +3,17 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const Schema = mongoose.Schema
 const server = express()
+const model= require("./config/model")
+const Cliente = model.Cliente
+const Item = model.Item
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 
 
-server.set('views', __dirname + '/views')
-server.set('view engine', 'jade')
+// server.set('views', __dirname + '/views')
+// server.set('view engine', 'jade')
 server.use(express.static(__dirname + '/public'))
-
-mongoose.connect('mongodb://localhost:27017/clientes')
-
-const clienteSchema = new Schema({
-    nome: String, 
-    idade: Number, 
-    sexo: {type: String, enum: ['M', 'F']}, 
-    dataNascimento: Date, 
-    email: String, 
-    endereco: String
-})
-const Cliente = mongoose.model('Cliente', clienteSchema)
 
 
 server.get('/', function(req, res){
@@ -37,7 +28,7 @@ server.get('/clientes', function(req, res) {
 
 server.get('/clientes/:id', function(req, res) {
     // console.log(req.params._id)
-    Cliente.find({_id: req.params.id}, function(err, cliente){
+    Cliente.findOne({_id: req.params.id}, function(err, cliente){
         // console.log(cliente)
         res.send(cliente)
     })
@@ -52,7 +43,7 @@ server.post('/clientes', function(req, res) {
 })
 
 server.put('/clientes/:id', function(req, res) {
-    var cliente = (new Cliente(req.body))
+    var cliente = req.body;
     Cliente.update({_id: req.params.id}, cliente, (err, raw) => {
         console.log(raw)
         res.sendStatus(200)
@@ -64,6 +55,85 @@ server.delete('/clientes/:id', function(req, res) {
         res.sendStatus(200)
     })
 })
+
+
+//Itens
+
+server.get('/itens', function(req, res) {
+    Item.find(function(err, itens){
+        res.send(itens)
+    })
+})
+
+server.get('/itens/:id', function(req, res) {
+    // console.log(req.params._id)
+    Item.findOne({_id: req.params.id}, function(err, item){
+        console.log(item)
+        res.send(item)
+    })
+})
+
+server.post('/itens', function(req, res) {
+    console.log(req.body)
+    var item = new Item(req.body)
+    console.log(item)
+    item.save(() => res.sendStatus(200))
+
+})
+
+server.put('/itens/:id', function(req, res) {
+    var item = req.body;
+    Item.update({_id: req.params.id}, item, (err, raw) => {
+        console.log(raw)
+        res.sendStatus(200)
+    })
+})
+
+server.delete('/itens/:id', function(req, res) {
+    Cliente.remove({_id: req.params.id}, () => {
+        res.sendStatus(200)
+    })
+})
+
+
+//Pedidos
+
+server.get('/clientes/:idCliente/pedidos', function(req, res) {
+    Cliente.findOne({_id: req.params.idCliente})
+            .populate("pedidos.item")
+            .exec(function(err, cliente){
+        res.send(cliente.pedidos)
+    })
+})
+
+server.get('/clientes/:idCliente/pedidos/:idPedido', function(req, res) {
+    Cliente.findOne({_id: req.params.idCliente}, function(err, cliente){
+        var pedido = cliente.pedidos.find((pedido) => pedido._id == req.params.idPedido);
+        res.send(pedido)
+    })
+})
+
+server.post('/clientes/:idCliente/pedidos', function(req, res) {
+    Cliente.update({_id: req.params.idCliente},
+                    {$push: {pedidos: req.body}},
+                    (err, raw) => res.send(200));
+
+})
+
+server.put('/clientes/:idCliente/pedidos/:idPedido', function(req, res) {
+    Cliente.update({_id: req.params.idCliente, "pedido._id": req.params.idPedido},
+                    {$set: {"pedidos.$": req.body}},
+                    (err, raw) => res.send(200));
+})
+
+server.delete('/clientes/:idCliente/pedidos/:idPedido', function(req, res) {
+    Cliente.update({_id: req.params.idCliente},
+                    {$pull: {pedidos: {_id: req.params.idPedido}}},
+                    (err, raw) => res.send(200));
+})
+
+
+
 
 server.listen(8000, function() {
     console.log('Executando')
